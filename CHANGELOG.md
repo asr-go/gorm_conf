d6a58083bc329fdcd65663996d82cd43d5ddbee2
surh97 <surunhao@gmail.com>
2019-10-31 16:49:50 +0800

    remove DeletedTime index

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,3 +9,7 @@
 ## Version 1.0.1 (2019-10-31)
 
 - UpdatedTime 修改为指针类型
+
+## Version 1.0.2 (2019-10-31)
+
+- DeletedTime 取消索引
--- models/model.go
+++ models/model.go
@@ -4,5 +4,5 @@ package models
 type Model struct {
 	CreatedTime int64  `gorm:"not null;type:bigint(13)"`
 	UpdatedTime *int64 `gorm:"type:bigint(13)"`
-	DeletedTime *int64 `gorm:"index;type:bigint(13)"`
+	DeletedTime *int64 `gorm:"type:bigint(13)"`
 }
